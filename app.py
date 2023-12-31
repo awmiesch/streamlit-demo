ebe5d60a76ddf416c5bae9e169a75a72c2f2dbe3
awmiesch <awmiesch@gmail.com>
2023-12-31 10:59:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,10 @@ import pandas as pd
 import requests
 
 
-def get_data():
-    conn = st.connection("STREAMLIT_APPS")
-    df = conn.query("select * from streamlit_apps.demo.fcst_rates")
-    return df
-
-
 st.title("Rate Forecast Data")
 
-st.dataframe(get_data())
+conn = st.connection("streamlit_apps")
+df = conn.query("select * from streamlit_apps.demo.fcst_rates")
+
+st.dataframe(df)
 
