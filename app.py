7fb09f31a41461de9a7fe32d990b74ccfdb8e2e7
awmiesch <awmiesch@gmail.com>
2023-12-31 10:51:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,15 @@
-import streamlit
-import pandas
+import streamlit as st
+import pandas as pd
 import requests
-import snowflake.connector
-from urllib.error import URLError
 
 
 def get_data():
-    conn = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    cur = conn.cursor()
-    cur.execute("select * from streamlit_apps.demo.fcst_rates")
-    rows = cur.fetchall()
-    return rows
+    conn = st.connection("STREAMLIT_APPS")
+    df = conn.query("select * from streamlit_apps.demo.fcst_rates")
+    return df
 
 
-streamlit.title("Rate Forecast Data")
+st.title("Rate Forecast Data")
 
-streamlit.header("Current Data:")
+st.dataframe(get_data())
 
-streamlit.dataframe(get_data())
-
-#my_cur = my_cnx.cursor()
-#my_cur.execute("select * from fruit_load_list")
-#my_data_rows = my_cur.fetchall()
-#streamlit.header("The fruit list contains:")
-#streamlit.dataframe(my_data_rows)
